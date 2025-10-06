d919dffb30bbdd924fcc561bc52f725a234f12dc
TienManh24092003 <sao24092003@gmail.com>
2025-10-06 15:42:37 +0700

    test CI

--- src/main/java/com/web/serviceImp/ProductServiceImp.java
+++ src/main/java/com/web/serviceImp/ProductServiceImp.java
@@ -57,7 +57,7 @@ public class ProductServiceImp implements ProductService {
             throw new MessageException("Không tìm thấy thương hiệu a");
         }
         if (tradeMark.isEmpty()) {
-            throw new MessageException("Không tìm thấy danh mục");
+            throw new MessageException("Không tìm thấy danh mục b");
         }
         product.setCreatedDate(new Date(System.currentTimeMillis()));
         product.setCreatedTime(new Time(System.currentTimeMillis()));
